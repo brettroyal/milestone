c27b46e34909064f39be4e4e2c72b6a7e0aafa41
brett royal <brett.r.sutton@gmail.com>
2017-06-17 12:38:36 -0400

    added correct host name

--- app.py
+++ app.py
@@ -40,5 +40,5 @@ def index():
 		
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    app.run(host='127.0.0.1', port=5000)
+    #app.run(debug=True)
+    app.run(host='whispering-fortress-85064.herokuapp.com', port=5000)
